3dbb80696f00495c6d278e2e753437a3fbe193cd
sourabhdarwai <sourabh.darwai@morrisonsplc.co.uk>
2025-08-29 04:46:38 +0530

    feat: set dynamic export for dashboard and transaction creation pages

--- src/app/(main)/transaction/create/page.jsx
+++ src/app/(main)/transaction/create/page.jsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 import React from 'react';
 import { getUserAccounts } from '@/actions/dashboard';
 import AddTransactionForm from '../_components/AddTransactionForm';
